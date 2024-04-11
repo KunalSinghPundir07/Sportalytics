258375dc84f9295beff1ca07418f67380abd6017
KunalSinghPundir07 <144855438+KunalSinghPundir07@users.noreply.github.com>
2024-04-10 23:59:51 -0400

    Updating function and report with top player information

--- 8_Sportalytics_Functions.sql
+++ 8_Sportalytics_Functions.sql
@@ -11,6 +11,33 @@ BEGIN
     RETURN IsValid; -- Returning the count of rows satisfying the condition
 END;
 
+-- function to create Top Player by Awards & Top Player by Earnings
+
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE FUNCTION get_top_players_info RETURN SYS_REFCURSOR IS
+    combined_info_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN combined_info_cursor FOR
+    SELECT 'Top Player by Awards' AS category, player_id, player_firstname, player_lastname, 
+           nationality, position, team_id, no_of_awards AS metric
+    FROM players
+    WHERE no_of_awards = (SELECT MAX(no_of_awards) FROM players)
+    UNION ALL
+    SELECT 'Top Player by Earnings' AS category, player_id, player_firstname, player_lastname, 
+           nationality, position, team_id, (players_salary + players_salarybonus) AS metric
+    FROM players
+    WHERE (players_salary + players_salarybonus) = (
+        SELECT MAX(players_salary + players_salarybonus) FROM players
+    );
+    
+    RETURN combined_info_cursor;
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Error retrieving combined top players info: ' || SQLERRM);
+END get_top_players_info;
+/
+
+
 
 
 
