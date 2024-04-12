b27980ffef237e724a9e528ac9ad7e236d6de1d4
PoorvikaGirishBabu <156874913+PoorvikaGirishBabu@users.noreply.github.com>
2024-04-11 23:44:46 -0400

    Adding function and report to give the list of experienced coaches and to calculate the total salary for a given team based on the sum of the player's salaries and salary bonus

--- 8_Sportalytics_Functions.sql
+++ 8_Sportalytics_Functions.sql
@@ -10,8 +10,40 @@ BEGIN
 
     RETURN IsValid; -- Returning the count of rows satisfying the condition
 END;
+/
 
+--function to give the list of experienced coaches:
+CREATE OR REPLACE FUNCTION GetExperiencedCoaches(
+    YearsOfExperienceThreshold INT
+)
+RETURN SYS_REFCURSOR
+AS
+    experienced_coaches SYS_REFCURSOR;
+BEGIN
+    OPEN experienced_coaches FOR
+        SELECT COACH_ID, COACH_NAME, COACH_YEARSOFEXPERIENCE
+        FROM Coaches
+        WHERE COACH_YEARSOFEXPERIENCE > YearsOfExperienceThreshold;
 
+    RETURN experienced_coaches;
+END;
+/
+
+--function to calculate the total salary for a given team based on the sum of the player's salaries and salary bonuses
+CREATE OR REPLACE FUNCTION CalculateTotalSalaryForTeam(
+    TeamID INT
+)
+RETURN DECIMAL
+IS
+  TotalSalary DECIMAL(10,2);
+BEGIN
+    SELECT SUM(PLAYERS_SALARY + PLAYERS_SALARYBONUS) INTO TotalSalary
+    FROM Players
+    WHERE TEAM_ID = TeamID;
+
+    RETURN TotalSalary;
+END;
+/
 
 
 
